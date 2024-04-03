61f577dad22d2f7cfa12960fce06f7514f66131c
luis miguel <luis_robles@dlsu.edu.ph>
2024-04-04 00:21:18 +0800

    atlas_uri

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI || 'mongodb://localhost:27017/mydatabase';
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
